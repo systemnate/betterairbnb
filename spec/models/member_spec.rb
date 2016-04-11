144b28a02ba53d030c3dd85b10c8aaa7da4ede35
Nate Dalo <nate.dalo@yahoo.com>
2016-04-11 10:59:52 -0500

    member can register for site

---
+++ spec/models/member_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Member, type: :model do
+  describe "validations" do
+    it "has a valid factory" do
+      expect(FactoryGirl.create(:member)).to be_valid
+    end
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
+end
